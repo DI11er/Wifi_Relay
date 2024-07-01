aebcef0f7b7a4a76bac400d47a4bf9b8dc2cd8cc
DI11er <80587689+DI11er@users.noreply.github.com>
2024-07-01 10:53:15 +0300

    Update relay.h

--- Client/ESP8266/WebSocketClientSocketIO/relay.h
+++ Client/ESP8266/WebSocketClientSocketIO/relay.h
@@ -6,12 +6,12 @@ class RelayManager {  // класс RelayManager
   public:
     // список членов, доступных в программе
     RelayManager(byte pin, bool state=true); // конструктор
-    void toggle(); // метод для переключения состояния сенсера
-    void set_state(bool state); // метод для установки состояния сенсера
+    void toggle(); // метод для переключения состояния реле
+    void set_state(bool state); // метод для установки состояния реле
     void restart();
     bool get_state(); // метод для проверки состояния реле
   private:
     // список членов для использования внутри класса
     byte _pin;  // номер пина
-    bool _state; // состояние сенсера
+    bool _state; // состояние реле
 };
